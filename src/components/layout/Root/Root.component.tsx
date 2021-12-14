f5503b781e16b89de3120034e7efbdee5970626b
Matheus Santos <matheus.psantos2016@gmail.com>
2021-12-14 20:43:49 -0300

    feat: add fonts to project

--- src/components/layout/Root/Root.component.tsx
+++ src/components/layout/Root/Root.component.tsx
@@ -1,5 +1,12 @@
 import React, { ReactNode } from "react";
 import { ThemeProvider } from "styled-components/native";
+import AppLoading from "expo-app-loading";
+import {
+  useFonts,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_700Bold,
+} from "@expo-google-fonts/poppins";
 
 import theme from "../../../core/styles/theme";
 
@@ -8,6 +15,14 @@ interface RootProps {
 }
 
 function Root({ children }: RootProps) {
+  const [isFontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold,
+  });
+
+  if (!isFontsLoaded) return <AppLoading />;
+
   return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
 }
 
